911569c5a9c65f30de3765a5f63bd10b57906b5c
NikhilMadaan45 <nikhilmadaan45@gmail.com>
2025-03-14 19:28:52 +0530

    Counter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [counter, setCounter] = useState(10);
+
+  const addValue = () => {
+    if(counter<20) {
+      setCounter(counter+1)
+    }
+  }
+
+  const subtractValue = () => {
+    if(counter>0) {
+      setCounter(counter-1)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Counter value: {counter}</h3>
+
+      <button
+        onClick={addValue}
+      > 
+      {counter >= 20 && <span>You reached the max limit</span>}
+      {counter < 20 && <span>Add 1</span>}
+      </button>
+      <br/>
+      <button
+        onClick={subtractValue}
+      > 
+      {counter <= 0 && <span>You reached the min limit</span>}
+      {counter > 0 && <span>Subtract 1</span>}
+      </button>
+      
     </>
   )
 }
